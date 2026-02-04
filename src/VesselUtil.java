e0ff329611e2a126c1593da39803f0a6d8c248bb
Nehaa-karthikeyan <nehaakart2004@gmail.com>
2026-02-04 11:11:50 +0530

    [Nehaa] :[Added] : UC5: Fixed UserInterface integration and completed console application.

--- src/VesselUtil.java
+++ src/VesselUtil.java
@@ -1,21 +1,40 @@
-public List<Vessel> getHighPerformanceVessels() {
+import java.util.ArrayList;
+import java.util.List;
 
-    List<Vessel> highPerformanceList = new ArrayList<>();
-    double maxSpeed = 0;
+public class VesselUtil {
 
-    // Find maximum speed
-    for (Vessel v : vesselList) {
-        if (v.getAverageSpeed() > maxSpeed) {
-            maxSpeed = v.getAverageSpeed();
-        }
+    private List<Vessel> vesselList = new ArrayList<>();
+
+    public void addVesselPerformance(Vessel vessel) {
+        vesselList.add(vessel);
     }
 
-    // Collect vessels with max speed
-    for (Vessel v : vesselList) {
-        if (v.getAverageSpeed() == maxSpeed) {
-            highPerformanceList.add(v);
+    public Vessel getVesselById(String vesselId) {
+        for (Vessel v : vesselList) {
+            if (v.getVesselId().equals(vesselId)) {
+                return v;
+            }
         }
+        return null;
     }
 
-    return highPerformanceList;
+    public List<Vessel> getHighPerformanceVessels() {
+
+        List<Vessel> highPerformanceList = new ArrayList<>();
+        double maxSpeed = 0;
+
+        for (Vessel v : vesselList) {
+            if (v.getAverageSpeed() > maxSpeed) {
+                maxSpeed = v.getAverageSpeed();
+            }
+        }
+
+        for (Vessel v : vesselList) {
+            if (v.getAverageSpeed() == maxSpeed) {
+                highPerformanceList.add(v);
+            }
+        }
+
+        return highPerformanceList;
+    }
 }
