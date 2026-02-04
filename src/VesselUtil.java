15481586cdcc3e95f9d7d46421160c060ec46820
Nehaa-karthikeyan <nehaakart2004@gmail.com>
2026-02-04 11:03:06 +0530

    [Nehaa] : [Added] : UC3: Implemented search functionality to retrieve vessel by ID.

--- src/VesselUtil.java
+++ src/VesselUtil.java
@@ -1,11 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class VesselUtil {
-
-    private List<Vessel> vesselList = new ArrayList<>();
-
-    public void addVesselPerformance(Vessel vessel) {
-        vesselList.add(vessel);
+public Vessel getVesselById(String vesselId) {
+    for (Vessel v : vesselList) {
+        if (v.getVesselId().equals(vesselId)) {
+            return v;
+        }
     }
+    return null;
 }
